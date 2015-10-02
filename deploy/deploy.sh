0fa01dd39845f6ab95472f0bb25b19589e2af084
Ramu Pulipati <ramu@rohilabs.com>
2015-10-02 04:59:05 -0400

    WH-34 Added whmgmt database create scripts

---
+++ deploy/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Run as root
+# http://askubuntu.com/questions/15853/how-can-a-script-check-if-its-being-run-as-root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
